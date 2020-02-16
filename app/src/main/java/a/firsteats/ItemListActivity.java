6c6fdc9dda02467ee6ca0ceedb77f2cb36359dda
Nick <fabdadv@gmai.com>
2020-02-16 12:38:56 +0700

    Changed Redundant Androidx Libs

--- app/src/main/java/a/firsteats/ItemListActivity.java
+++ app/src/main/java/a/firsteats/ItemListActivity.java
@@ -110,7 +110,7 @@ public class ItemListActivity extends AppCompatActivity {
 
         @Override
         @NonNull
-        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        public ViewHolder   onCreateViewHolder(ViewGroup parent, int viewType) {
             View view = LayoutInflater.from(parent.getContext())
                     .inflate(R.layout.item_list_content, parent, false);
             return new ViewHolder(view);
